5946c375934bfd609d556116d38bbaeb2df61dbf
YaregBel <yaroslav03bel@gmail.com>
2024-09-24 04:01:01 +0800

    Adding files from local project

---
+++ build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_pdfViewer_FILE D:/Projects/pdfViewer/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/pdfViewer.exe)
+set(__QT_DEPLOY_TARGET_pdfViewer_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_pdfViewer_RUNTIME_DLLS D:/progs/QT/6.7.2/mingw_64/bin/Qt6Widgets.dll;D:/progs/QT/6.7.2/mingw_64/bin/Qt6Gui.dll;D:/progs/QT/6.7.2/mingw_64/bin/Qt6Core.dll)
